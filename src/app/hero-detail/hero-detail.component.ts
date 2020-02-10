8fa3ce6c62c496c0b3e4bdab32ee81f7804acc53
Pato <pevargas2730@gmail.com>
2020-02-09 19:43:24 -0500

    addying async code

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
 
-import { Hero }         from '../hero';
-import { HeroService }  from '../hero.service';
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  selector: "app-hero-detail",
+  templateUrl: "./hero-detail.component.html",
+  styleUrls: ["./hero-detail.component.css"]
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
@@ -24,17 +24,45 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
   }
 
   goBack(): void {
     this.location.back();
   }
 
- save(): void {
-    this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
+  save(): void {
+    debounce(
+      //this makes our code async
+      () => {
+        this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+      },
+      250,
+      false
+    )();
   }
+}
+
+function debounce(func, wait, immediate) {
+  //makes sure a funtion doesnt get call to often
+  let timeout;
+
+  return function() {
+    const context = this,
+      args = arguments;
+    const later = function() {
+      timeout = null;
+
+      if (!immediate) func.apply(context, args);
+    };
+
+    const callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
+  };
 }
